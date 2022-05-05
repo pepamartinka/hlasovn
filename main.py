557ff8eee011065465cfd2c73ef29600044343c0
pepamartinka <91247604+pepamartinka@users.noreply.github.com>
2022-05-05 11:28:47 +0200

    škola-vlastik-spolupráce

--- main.py
+++ main.py
@@ -1,40 +1,72 @@
-radio.set_group(69)
+radio.set_group(666)
 radio.set_transmit_power(7)
 radio.set_transmit_serial_number(True)
 seriovy = control.device_serial_number()
 
 enable=0
 
-print(seriovy)
+
+def on_received_value(name, value):
+    global enable
+    if name=="ready" and value== 1:
+        basic.show_leds("""
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        . . . . .
+        """)
+        enable=1
+        basic.pause(2000)
+        basic.clear_screen()
+
+    if name=="notready" and value== 0:
+        basic.show_leds("""
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        """)
+        enable=0
+        basic.pause(2000)
+        basic.clear_screen()
+radio.on_received_value(on_received_value)
+
 
 def on_button_pressed_a():
-     radio.send_value("vote",1)
-     music.play_tone(Note.C, music.beat(300))
-     basic.show_string("A")
+        if enable ==1:
+            radio.send_value("hlas",0)
+            music.play_tone(Note.C, music.beat(300))
+            basic.show_string("A")
+            basic.clear_screen()
+
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    radio.send_value("vote", 2)
+    if enable ==1:
+        radio.send_value("hlas", 1)
+        music.play_tone(Note.C, music.beat(300))
+        basic.show_string("B")
+        basic.clear_screen()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_pin_pressed_p0():
-    radio.send_value("vote", 3)
+    if enable ==1:
+        radio.send_value("hlas", 2)
+        music.play_tone(Note.C, music.beat(300))
+        basic.show_string("C")
+        basic.clear_screen()
 input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
 
 def on_pin_pressed_p2():
-    radio.send_value("vote", 4)
-input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+    if enable ==1:
+        radio.send_value("hlas", 3)
+        music.play_tone(Note.C, music.beat(300))
+        basic.show_string("D")
+        basic.clear_screen()
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
 
 
-def on_received_value(name, value):
-    pocetA=0
-    pocetB=0
-    pocetC=0
-    pocetD=0
-    if name == "vote" and value == 1:
-        pocetA +=1
 
-    if name == "vote" and value == 2:
-        pocetB+=1
-radio.on_received_value(on_received_value)
 
--- main.ts
+++ main.ts
@@ -1,38 +1,70 @@
-radio.setGroup(69)
+radio.setGroup(666)
 radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
 let seriovy = control.deviceSerialNumber()
 let enable = 0
-console.log(seriovy)
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendValue("vote", 1)
-    music.playTone(Note.C, music.beat(300))
-    basic.showString("A")
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    radio.sendValue("vote", 2)
-})
-function on_pin_pressed_p0() {
-    radio.sendValue("vote", 3)
-}
-
-input.onPinPressed(TouchPin.P0, on_pin_pressed_p0)
-function on_pin_pressed_p2() {
-    radio.sendValue("vote", 4)
-}
-
-input.onPinPressed(TouchPin.P0, on_pin_pressed_p0)
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    let pocetA = 0
-    let pocetB = 0
-    let pocetC = 0
-    let pocetD = 0
-    if (name == "vote" && value == 1) {
-        pocetA += 1
+    
+    if (name == "ready" && value == 1) {
+        basic.showLeds(`
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        . . . . .
+        `)
+        enable = 1
+        basic.pause(2000)
+        basic.clearScreen()
     }
     
-    if (name == "vote" && value == 2) {
-        pocetB += 1
+    if (name == "notready" && value == 0) {
+        basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+        enable = 0
+        basic.pause(2000)
+        basic.clearScreen()
+    }
+    
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    if (enable == 1) {
+        radio.sendValue("hlas", 0)
+        music.playTone(Note.C, music.beat(300))
+        basic.showString("A")
+        basic.clearScreen()
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    if (enable == 1) {
+        radio.sendValue("hlas", 1)
+        music.playTone(Note.C, music.beat(300))
+        basic.showString("B")
+        basic.clearScreen()
+    }
+    
+})
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    if (enable == 1) {
+        radio.sendValue("hlas", 2)
+        music.playTone(Note.C, music.beat(300))
+        basic.showString("C")
+        basic.clearScreen()
+    }
+    
+})
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    if (enable == 1) {
+        radio.sendValue("hlas", 3)
+        music.playTone(Note.C, music.beat(300))
+        basic.showString("D")
+        basic.clearScreen()
     }
     
 })
